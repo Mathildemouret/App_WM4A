85078ca2bbc05488d70e10a9185407e939bb3da0
Mathildemouret <48008464+Mathildemouret@users.noreply.github.com>
2019-04-13 19:32:25 +0200

    Delete form.js

--- js/form.js
+++
@@ -1,9 +0,0 @@
-
-
-document.addEventListener("DOMContentLoaded",function(event){
-    console.log("Le dom est chargé");
-    var btn = document.getElementById("btn");
-    var form = document.getElementById("form");
-    btn.addEventListener("click",alert("Enregistré!"));
-    form.addEventListener("click",form.reset());
-});
